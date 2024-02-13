c04e9009a348d2bc743ce28973e6b1fba3100c62
dajver <lfqdth5@gmail.com>
2024-02-13 14:50:40 +0200

    added smooth scroller

--- app/src/main/java/com/a1hd/movies/ui/sections/dashboard/manager/TVLinearLayoutManager.kt
+++ app/src/main/java/com/a1hd/movies/ui/sections/dashboard/manager/TVLinearLayoutManager.kt
@@ -2,8 +2,11 @@ package com.a1hd.movies.ui.sections.dashboard.manager
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.DisplayMetrics
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSmoothScroller
+import androidx.recyclerview.widget.RecyclerView
 
 class TVLinearLayoutManager : LinearLayoutManager {
 
@@ -20,5 +23,33 @@ class TVLinearLayoutManager : LinearLayoutManager {
                 return focused
         }
         return super.onInterceptFocusSearch(focused, direction)
+    }
+
+    override fun smoothScrollToPosition(
+        recyclerView: RecyclerView,
+        state: RecyclerView.State,
+        position: Int
+    ) {
+        val centerSmoothScroller = CenterSmoothScroller(recyclerView.context)
+        centerSmoothScroller.targetPosition = position
+        startSmoothScroll(centerSmoothScroller)
+    }
+}
+class CenterSmoothScroller(context: Context) : LinearSmoothScroller(context) {
+
+    override fun calculateDtToFit(
+        viewStart: Int,
+        viewEnd: Int,
+        boxStart: Int,
+        boxEnd: Int,
+        snapPreference: Int
+    ): Int = (boxStart + (boxEnd - boxStart) / 2) - (viewStart + (viewEnd - viewStart) / 2)
+
+    override fun calculateSpeedPerPixel(displayMetrics: DisplayMetrics): Float {
+        return MILLISECONDS_PER_INCH / displayMetrics.densityDpi
+    }
+
+    companion object {
+        private const val MILLISECONDS_PER_INCH = 150f
     }
 }
